071c81cec62ab080924f18917b53b4f11823e769
Tony Guntharp <fusion94@damagestudios.net>
2013-01-22 05:34:00 -0800

    Update HMC5883L_Arduino/HMC5883L_Arduino.ino

--- HMC5883L_Arduino/HMC5883L_Arduino.ino
+++ HMC5883L_Arduino/HMC5883L_Arduino.ino
@@ -29,15 +29,19 @@ void setup() {
   Wire.write(byte(0x02));
   Wire.write(byte(0x00));
   Wire.endTransmission();
+}  if(Wire.available() <=6) {    // If 6 bytes available
+    x = Wire.read() << 8 | Wire.read();
+    z = Wire.read() << 8 | Wire.read();
+    y = Wire.read() << 8 | Wire.read();
+  }
+  
+  // Print raw values
+  Serial.print("X=");
+  Serial.print(x);
+  Serial.print(", Y=");
+  Serial.print(y);
+  Serial.print(", Z=");
+  Serial.println(z);
+  
+  delay(500);
 }
-
-void loop() {
-  int x, y, z;
-
-  // Initiate communications with compass
-  Wire.beginTransmission(Addr);
-  Wire.write(byte(0x03));       // Send request to X MSB register
-  Wire.endTransmission();
-
-  Wire.requestFrom(Addr, 6);    // Request 6 bytes; 2 bytes per axis
-  if(Wire.available()
